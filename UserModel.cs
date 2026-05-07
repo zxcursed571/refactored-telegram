20dd20fd44e4b790320640280ac8c07c429bb54e
Студент Колледжа <klg@top.corp>
2026-05-07 12:40:27 +0500

    Add user authentication service and model

---
+++ UserModel.cs
@@ -0,0 +1,9 @@
+namespace CSharp;
+
+public class UserModel
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+}
